be6a38c37bc8b6e73b0bf6a3e49cada8425dd4da
michelleconville <michelle.conville@gmail.com>
2023-07-01 17:48:39 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NP7V1IAQ5RnOlLE0L6ESJsvhecBet0o0N0Yl',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
